4bb9600498d4886f64e3891555725cabc470aa55
Bose, Soumyadeep <soumyadeep.work01@gmail.com>
2023-02-01 03:42:51 +0530

    Code for Day 3_treasure-island-end project and the flowchart for development
    Signed-off-by: Bose, Soumyadeep <soumyadeep.work01@gmail.com>

---
+++ Day 3_treasure-island-end/main.py
@@ -0,0 +1,27 @@
+'''
+This is a mini game on a treasure island.
+Choose the right option to reach to the treasure!
+'''
+import mako.filters
+
+print("Welcome to Treasure Island.")
+print('Your mission is to find the treasure')
+direction_to_take = input("You're at a cross road. Where do you want to go? Type \"left\" or \"right\" ").strip()
+if direction_to_take == 'left':
+	print('You come to a lake. There is an island in the middle of the lake', end = ' ')
+	boat_or_swim = input('Type "wait" to wait for a boat. Type "swim" to swim across ').strip()
+	if boat_or_swim != 'wait':
+		print('You get attacked by an angry trout!',"Game Over.")
+	else:
+		print("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue.", end = ' ')
+		color_chosen = input("Which colour do you choose? ").strip().capitalize()
+		if color_chosen == 'Red':
+			print('You got burned by fire!', 'Game Over.')
+		elif color_chosen == 'Blue':
+			print('You are eaten by beasts!', 'Game Over.')
+		elif color_chosen == 'Yellow':
+			print('You Win!')
+		else:
+			print('Game Over.')
+else:
+	print('You fell into a hole! Game Over.')
