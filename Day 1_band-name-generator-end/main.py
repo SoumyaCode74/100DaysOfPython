3c11bb7cf3bf02805571115e578ddaa488842519
Bose, Soumyadeep <soumyadeep.work01@gmail.com>
2023-02-01 02:16:03 +0530

    Code for Day 1_band-name-generator-end project
    Signed-off-by: Bose, Soumyadeep <soumyadeep.work01@gmail.com>

---
+++ Day 1_band-name-generator-end/main.py
@@ -0,0 +1,16 @@
+'''
+Required output:
+Welcome to the Band Name Generator
+What's the name of the city you grew up in?
+> Kolkata
+What's your pet's name?
+> Pooch
+Your band name could be Kolkata pooch
+
+'''
+
+print('Welcome to the Band Name Generator')
+city    = input("What's the name of the city you grew up in?\n")
+pet     = input("What's your pet's name?\n")
+print(f'Your band name could be {city} {pet}')
+
